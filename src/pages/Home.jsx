4b35ac395708ccf253e999f6cbe46494be4c2728
dhirajbantawarai <61051104+dhirajbantawarai@users.noreply.github.com>
2024-05-14 15:27:03 +0545

    nav ko kam haru + home ma kei

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,23 @@
 import React from "react";
 
+import Hero from "../components/Hero/Hero";
+import MainVid from "../assets/video/main.mp4";
 const Home = () => {
   return (
     <>
-      <div>Home</div>
+      <div>
+        <div className="relative h-[700px]">
+          <video
+            autoPlay
+            loop
+            muted
+            className="absolute right-0 top-0 h-[700px] w-full object-cover z-[-1]"
+          >
+            <source src={MainVid} type="video/mp4" />
+          </video>
+          <Hero />
+        </div>
+      </div>
     </>
   );
 };
